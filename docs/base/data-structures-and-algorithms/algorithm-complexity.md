67d2042b2914984b5c9e2746c1af7a7bc46d0b10
lujing <lujing@zhixuezhen.com>
2021-06-16 17:36:02 +0800

    feat: add algorithm-complexity

--- docs/base/data-structures-and-algorithms/algorithm-complexity.md
+++ docs/base/data-structures-and-algorithms/algorithm-complexity.md
@@ -3,3 +3,58 @@ sidebar: auto
 ---
 
 # 算法复杂度
+
+## 大O表示法
+
+它用于描述算法的性能和复杂程度。大O表示法将算法按照消耗的时间进行分类，依据随输入增大所需要的空间/内存
+
+符号 | 名称
+---|---
+O(1) | 常数的
+O(㏒(n)) | 对数的
+O((㏒(n))c) | 对数多项式的
+O(n) | 线性的
+O(n²) | 二次的
+O(n^ｃ) | 多项式的
+O(c^ｎ) | 指数的
+
+如何衡量算法的效率？通常是用资源，例如 CPU（时间）占用、内存占用、硬盘占用和网络占用。当讨论大 O 表示法时，一般考虑的是 CPU（时间）占用
+
+### O(1)
+
+```js
+function increment(num) {
+  return ++num
+}
+```
+
+### O(n)
+
+```js
+function sequentialSearch(array, value, equalsFn = defaultEquals) {
+  for (let i = 0; i < array.length; i++) {
+    if (equalsFn(value, array[i])) {
+      return i
+    }
+  }
+  return -1
+}
+```
+
+### O(n²)
+
+```js
+function bubbleSort(array, compareFn = defaultCompare) {
+  const { length } = array
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - 1; j++) {
+      if (compareFn(array[j], array[j + 1]) === Compare.BIGGER_THAN) {
+        swap(array, j, j + 1)
+      }
+    }
+  }
+  return array
+}
+```
+
+### 时间复杂度比较
--- docs/test/index.js
+++ docs/test/index.js
@@ -1,48 +1,11 @@
-const Compare = {
-  LESS_THAN: -1,
-  BIGGER_THAN: 1,
-}
-function defaultCompare(a, b) {
-  if (a === b) {
-    return 0
-  }
-  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN
-}
-// 交换函数
-function swap(array, a, b) {
-  const temp = array[a]
-  array[a] = array[b]
-  array[b] = temp
-}
-function countingSort(array) {
-  if (array.length < 2) {
-    return array
-  }
-  const maxValue = findMaxValue(array)
-  const counts = new Array(maxValue + 1)
-  array.forEach(element => {
-    if (!counts[element]) {
-      counts[element] = 0
+function bubbleSort(array, compareFn = defaultCompare) {
+  const { length } = array
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - 1; j++) {
+      if (compareFn(array[j], array[j + 1]) === Compare.BIGGER_THAN) {
+        swap(array, j, j + 1)
+      }
     }
-    counts[element]++
-  })
-  let sortedIndex = 0
-  counts.forEach((count, i) => {
-    while (count > 0) {
-      array[sortedIndex++] = i
-      count--
-    }
-  })
+  }
   return array
 }
-function findMaxValue(array) {
-  let max = array[0]
-  for (let i = 1; i < array.length; i++) {
-    if (array[i] > max) {
-      max = array[i]
-    }
-  }
-  return max
-}
-const arr = [3, 5, 1, 6, 4, 7, 2]
-console.log(countingSort(arr))
