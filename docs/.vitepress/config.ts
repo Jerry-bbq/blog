2e78f20cf22cd496314a17d3c6741d4f9715dd21
Jerry <lujing2457@163.com>
2024-02-20 18:39:25 +0800

    feat: 开源

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -112,6 +112,15 @@ function nav(): DefaultTheme.NavItem[] {
       link: '/share/npm/index',
       activeMatch: '/share/',
     },
+    {
+      text: '开源',
+      items: [
+        { text: '工具函数库lgr-tools', link: 'https://www.npmjs.com/package/lgr-tools' },
+        { text: 'circle-slider', link: 'https://ext.dcloud.net.cn/plugin?id=8084' },
+        { text: '仪表盘或者环形进度条', link: 'https://ext.dcloud.net.cn/plugin?id=8089' },
+        { text: 'vue-img-verify', link: 'https://github.com/Jerry-bbq/vue-img-verify' }
+      ],
+    },
     // {
     //   text: 'HK-Office',
     //   link: '/audition/index',
