09e57e53ee22ed66f0cfbb2db112a5e5ccdf8b32
Jerry <lujing2457@163.com>
2024-09-01 22:54:43 +0800

    feat: add medium-zoom and config

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,10 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
+import { h, onMounted, watch, nextTick } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+import mediumZoom from 'medium-zoom'
+import { useRoute } from 'vitepress'
 
 export default {
   extends: DefaultTheme,
@@ -13,5 +15,18 @@ export default {
   },
   enhanceApp({ app, router, siteData }) {
     // ...
-  }
+  },
+  setup() {
+    const route = useRoute()
+    const initZoom = () => {
+      mediumZoom('#app img', { background: 'var(--vp-c-bg)' })
+    }
+    onMounted(() => {
+      initZoom()
+    })
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
+  },
 } satisfies Theme
